b440c88866755855d4ef38e383e99d929d6d9d40
Maksim_Stl <qamaksims@gmail.com>
2024-02-03 11:36:45 +0100

    file hw13_t_01.py  added

--- homework/maksim_stulau/homework_13/hw13_t_01.py
+++ homework/maksim_stulau/homework_13/hw13_t_01.py
@@ -24,14 +24,26 @@ print(eugene_file_path)
 
 
 with open(eugene_file_path) as data_file:
-    print(data_file.read())
+    for line in data_file.readlines():
+        number = int(line.split('.')[0])
+        date_str = line[3:29]
+        date = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S.%f')
+        if number == 1:
+            print(date + datetime.timedelta(days=7))
+        elif number == 2:
+            print(date.strftime('%A'))
+        elif number == 3:
+            today_time = datetime.datetime.now()
+            num_days = (today_time - date).days
+            print(num_days)
 
-today_time = datetime.datetime.now()
-print(today_time)
-min_week = today_time + datetime.timedelta(days=7)
-print(f"1. {min_week}")
-name_day = today_time.strftime("%A")
-print(f"2. {name_day}")
-last_date = datetime.datetime(year=2023, month=6, day=12, hour=15, minute=23, second=45, microsecond=312167)
-num_days = today_time - last_date
-print(f"3. {num_days}")
+
+# today_time = datetime.datetime.now()
+# print(today_time)
+# min_week = today_time + datetime.timedelta(days=7)
+# print(f"1. {min_week}")
+# name_day = today_time.strftime("%A")
+# print(f"2. {name_day}")
+# last_date = datetime.datetime(year=2023, month=6, day=12, hour=15, minute=23, second=45, microsecond=312167)
+# num_days = today_time - last_date
+# print(f"3. {num_days}")
