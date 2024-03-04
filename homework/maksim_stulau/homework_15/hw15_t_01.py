d8643545240ec39f55b9b1647c55c11344e4c6ed
Maksim_Stl <qamaksims@gmail.com>
2024-03-04 22:50:35 +0100

    file hw15_t_01 fixed

--- homework/maksim_stulau/homework_15/hw15_t_01.py
+++ homework/maksim_stulau/homework_15/hw15_t_01.py
@@ -10,17 +10,50 @@ db = mysql.connect(
 
 
 cursor = db.cursor()
-cursor.execute("INSERT INTO `groups`(title, start_date, end_date) values ('C2C_02', 'okt 2023', 'aprl 2024')")
+query = "INSERT INTO `groups` (title, start_date, end_date) VALUES (%s, %s, %s)"
+values = ('C2C_02', 'okt 2023', 'aprl 2024')
+cursor.execute(query, values)
 group_id = cursor.lastrowid
 print(group_id)
-cursor.execute(f"insert INTO students (name, second_name, group_id) VALUES ('GLEB', 'SARATAS', {group_id})")
+
+query = "insert INTO students (name, second_name, group_id) VALUES (%s, %s, %s)"
+values = ('GLEB', 'SARATAS', group_id)
+cursor.execute(query, values)
 student_id = cursor.lastrowid
-cursor.execute(f"INSERT INTO books (title, taken_by_student_id) values ('Second', {student_id})")
-cursor.execute("INSERT  into subjets (title) values ('Bukvar')")
+
+query = "INSERT INTO books (title, taken_by_student_id) values (%s, %s)"
+values = ('Second', student_id)
+cursor.execute(query, values)
+
+query = "INSERT  into subjets (title) values (%s)"
+values = ('Bukvar',)
+cursor.execute(query, values)
 subject_id = cursor.lastrowid
-cursor.execute(f"INSERT  INTO lessons (title, subject_id) values ('lesson 201', {subject_id})")
+
+query = "INSERT  INTO lessons (title, subject_id) values (%s, %s)"
+values = ('lesson 201', subject_id)
+cursor.execute(query, values)
 lesson_id = cursor.lastrowid
-cursor.execute(f"INSERT INTO marks (value, lesson_id, student_id) values ('DONE', {lesson_id}, {student_id})")
+
+query = "INSERT INTO marks (value, lesson_id, student_id) values (%s, %s, %s)"
+values = ('DONE', lesson_id, student_id)
+cursor.execute(query, values)
 
 db.commit()
+
+cursor.execute("""
+    SELECT students.name, `groups`.title as `group`, books.title as book,
+           marks.value, subjets.title as sub, lessons.title as less
+    from students
+    join `groups` on students.group_id = `groups`.id
+    join books on books.taken_by_student_id = students.id
+    join marks on marks.student_id = students.id
+    JOIN lessons on lessons.id = marks.lesson_id
+    JOIN subjets on lessons.subject_id = subjets.id
+    where students.name = 'GLEB'
+""")
+result = cursor.fetchone()
+print("Results of the joined data:")
+for data in result:
+    print(data)
 db.close()
